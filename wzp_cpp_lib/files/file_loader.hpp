a521234819552ebe472fac44257815f5dec1d09e
wzppengpeng <wzpycg@qq.com>
2017-05-15 20:53:38 +0800

    use STL iterator type to add a new fileload, can use for(auto : loader), more conveients

--- wzp_cpp_lib/files/file_loader.hpp
+++ wzp_cpp_lib/files/file_loader.hpp
@@ -60,6 +60,108 @@ private:
 };
 
 
+/**
+ * STL iterator type file loader
+ */
+//use just like STL
+//return an iterator point return data
+template<typename R>
+class FileLoad
+{
+    class FileIter;
+    typedef long long ll;
+
+public:
+    FileLoad() : m_can_iter(false)
+    {}
+
+    //the regis function, to regis a function object to get process a line
+    template<typename Fun>
+    void regis(Fun f, const std::string& filename) {
+        m_fn = std::move(f);
+        ifile.open(filename, std::ios::in);
+        if(!ifile) m_can_iter = false;
+        else m_can_iter = true;
+    }
+
+    //the begin
+    FileIter begin() {
+        //load first line
+        if(!m_can_iter) {
+            return FileIter(-1, *this);
+        }
+        if(std::getline(ifile, m_buffer)) {
+            return FileIter(0, *this);
+        }
+        else {
+            ifile.close();
+            return FileIter(-1, *this);
+        }
+    }
+
+    //the end
+    FileIter end() {
+        return FileIter(-1, *this);
+    }
+
+private:
+    bool m_can_iter;
+
+    //the function object
+    std::function<R(const std::string&)> m_fn;
+
+    //the file stream
+    std::ifstream ifile;
+    std::string m_buffer;
+
+    R m_r;
+
+private:
+
+    class FileIter
+    {
+    public:
+        //constructor
+        FileIter(ll idx, FileLoad& load) : m_idx(idx), m_load(load)
+        {
+        }
+
+        //dereferece
+        R& operator* () {
+            m_load.m_r = std::move(m_load.m_fn(m_load.m_buffer));
+            return m_load.m_r;
+        }
+
+        //self increament
+        FileIter& operator++ () {
+            //read next line
+            if(std::getline(m_load.ifile, m_load.m_buffer)) {
+                ++m_idx;
+            }
+            else {
+                m_load.m_can_iter = false;
+                m_idx = -1;
+                m_load.ifile.close();
+            }
+            return *this;
+        }
+
+        //==
+        bool operator== (const FileIter& other) {
+            return other.m_idx == this->m_idx;
+        }
+
+        bool operator!= (const FileIter& other) {
+            return other.m_idx != this->m_idx;
+        }
+
+    private:
+        ll m_idx;
+        FileLoad& m_load;
+    };
+};
+
+
 } //wzp
 
 #endif //FILE_LOADER_HPP_
