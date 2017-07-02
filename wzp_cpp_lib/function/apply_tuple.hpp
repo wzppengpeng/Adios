825daaa65cbc73d3f2e9a0c9a417004c015dd376
wzppengpeng <wzpycg@qq.com>
2017-07-02 13:28:18 +0800

    add apply for tuple

--- wzp_cpp_lib/function/apply_tuple.hpp
+++ wzp_cpp_lib/function/apply_tuple.hpp
@@ -3,7 +3,8 @@
 
 
 #include <tuple>
-
+#include <type_traits>
+#include <utility>
 
 namespace wzp
 {
@@ -29,6 +30,44 @@ struct FindIndex<0, T, Args...>
     }
 };
 
+
+/**
+ * use tuple to wrap the args, then apply them to a function object
+ */
+//get the N - 1 arg
+template<size_t N>
+struct Apply
+{
+    template<typename F, typename T, typename... Args>
+    inline static auto call(F&& f, T&& t, Args&&... a)
+    -> decltype(Apply<N - 1>::call(
+        std::forward<F>(f), std::forward<T>(t),
+        std::get<N - 1>(std::forward<T>(t)),
+        std::forward<Args>(a)...
+    )) {
+        return Apply<N - 1>::call(
+            std::forward<F>(f), std::forward<T>(t),
+            std::get<N - 1>(std::forward<T>(t)),
+            std::forward<Args>(a)...
+        );
+    }
+};
+
+//the special
+template<>
+struct Apply<0>
+{
+    template<typename F, typename T, typename... Args>
+    inline static auto call(F&& f, T&&, Args&&... a)
+    ->decltype(
+        std::forward<F>(f)
+        (std::forward<Args>(a)...)
+    ) {
+        return std::forward<F>(f)
+            (std::forward<Args>(a)...);
+    }
+};
+
 } //detail
 
 // the wrapper
@@ -36,6 +75,17 @@ struct FindIndex<0, T, Args...>
 template<typename T, typename... Args>
 int find_index(const std::tuple<Args...>& t, T&& val) {
     return detail::FindIndex<sizeof...(Args), T, Args...>::call(t, std::forward<T>(val));
+}
+
+//the wrapper of apply
+template<typename F, typename T>
+inline auto apply(F&& f, T&& t)
+-> decltype(
+    detail::Apply<std::tuple_size<typename std::decay<T>::type>::value>
+    ::call(std::forward<F>(f), std::forward<T>(t))
+) {
+    return detail::Apply<std::tuple_size<typename std::decay<T>::type>::value>
+        ::call(std::forward<F>(f), std::forward<T>(t));
 }
 
 } //wzp
