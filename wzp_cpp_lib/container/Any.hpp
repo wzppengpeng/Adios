c094b842384d947eba05f41966401a9900ace155
wzppengpeng <wzpycg@qq.com>
2018-04-11 16:35:44 +0800

    add my new any type, very easy to use

--- wzp_cpp_lib/container/Any.hpp
+++ wzp_cpp_lib/container/Any.hpp
@@ -1,82 +1,112 @@
-#pragma once
+#ifndef WZP_ANY_HPP_H_
+#define WZP_ANY_HPP_H_
+
+#include <string>
 #include <memory>
 #include <typeindex>
 
-namespace wzp {
-
-struct Any
+namespace wzp
 {
-	template<typename U> Any(U && value) : m_ptr(new Derived < typename decay<U>::type>(forward<U>(value))),
-	m_tpIndex(type_index(typeid(typename decay<U>::type))){}
 
-	Any(void) : m_tpIndex(std::type_index(typeid(void))){}
-	Any(Any& that) : m_ptr(that.Clone()), m_tpIndex(that.m_tpIndex) {}
-	Any(Any && that) : m_ptr(std::move(that.m_ptr)), m_tpIndex(that.m_tpIndex) {}
+class any {
+public:
+    // the exception for any cast
+    struct BadAnyCast : std::bad_cast {
+        BadAnyCast(const std::string& reason) :reason_(reason) {}
+        BadAnyCast(std::string&& reason) :reason_(std::move(reason)) {}
 
-	bool IsNull() const { return !bool(m_ptr); }
+        virtual const char* what() const noexcept {
+            return reason_.c_str();
+        }
 
-	template<class U> bool Is() const
-	{
-		return m_tpIndex == type_index(typeid(U));
-	}
+        std::string reason_;
+    };
 
-	template<class U>
-	U& AnyCast()
-	{
-		if (!Is<U>())
-		{
-			cout << "can not cast " << typeid(U).name() << " to " << m_tpIndex.name() << endl;
-			throw bad_cast();
-		}
+public:
+    // the any type constructor
+    template<typename T> any(T&& val) : ptr_(new Derived<typename std::decay<T>::type>(std::forward<T>(val))),
+        tp_index_(std::type_index(typeid(typename std::decay<T>::type))) {}
+    //the default
+    any(void) : tp_index_(std::type_index(typeid(void))) {}
+    // copy constructor
+    any(const any& other) : ptr_(other.clone()), tp_index_(other.tp_index_) {}
+    // move constructor
+    any(any&& other) : ptr_(std::move(other.ptr_)), tp_index_(other.tp_index_) {}
 
-		auto derived = dynamic_cast<Derived<U>*> (m_ptr.get());
-		return derived->m_value;
-	}
+    // judge if is null
+    bool is_null() const { return ptr_.get() == nullptr; }
+    // judge if is type A
+    template<typename U> bool is() const { return tp_index_ == std::type_index(typeid(U)); }
+    // cast function, get the detail information
+    template<typename T>
+    const T& cast() const {
+        if(!is<T>()) {
+            throw BadAnyCast(std::string("can not cast ") + typeid(T).name() + " to " + tp_index_.name());
+        }
+        return dynamic_cast<Derived<T>* >(ptr_.get())->value_;
+    }
+    // the left value
+    template<typename T>
+    T& cast() {
+        if(!is<T>()) {
+            throw BadAnyCast(std::string("can not cast ") + typeid(T).name() + " to " + tp_index_.name());
+        }
+        return dynamic_cast<Derived<T>* >(ptr_.get())->value_;
+    }
 
-	Any& operator=(const Any& a)
-	{
-		if (m_ptr == a.m_ptr)
-			return *this;
-
-		m_ptr = a.Clone();
-		m_tpIndex = a.m_tpIndex;
-		return *this;
-	}
+    // the operator = for copy
+    any& operator= (const any& other) {
+        if(this != &other) {
+            ptr_ = other.clone();
+            tp_index_ = other.tp_index_;
+        }
+        return *this;
+    }
+    // the operator = for move
+    any& operator= (any&& other) {
+        if(this != &other) {
+            ptr_ = std::move(other.ptr_);
+            tp_index_ = other.tp_index_;
+        }
+        return *this;
+    }
 
 private:
-	struct Base;
-	typedef std::unique_ptr<Base> BasePtr;
+    struct Base;
+    using BasePtr = std::unique_ptr<Base>;
 
-	struct Base
-	{
-		virtual ~Base() {}
-		virtual BasePtr Clone() const = 0;
-	};
+    struct Base {
+        virtual ~Base() {}
+        // clone another value, set the function to be const
+        virtual BasePtr clone() const = 0;
+    }; //Base vritual class
 
-	template<typename T>
-	struct Derived : Base
-	{
-		template<typename U>
-		Derived(U && value) : m_value(forward<U>(value)) { }
+    template<typename T>
+    struct Derived : Base {
+        // the constructor
+        template<typename U>
+        Derived(U&& u) : value_(std::forward<U>(u)) {}
+        // override the clone function
+        virtual BasePtr clone() const {
+            return BasePtr(new Derived<T>(value_)); //set the memory dynamic
+        }
 
-		BasePtr Clone() const
-		{
-			return BasePtr(new Derived<T>(m_value));
-		}
+        T value_;
+    };
 
-		T m_value;
-	};
+    // a clone interface for copy constructor
+    BasePtr clone() const {
+        if(ptr_.get()) {
+            return ptr_->clone();
+        }
+        return nullptr;
+    }
 
-	BasePtr Clone() const
-	{
-		if (m_ptr != nullptr)
-			return m_ptr->Clone();
+    BasePtr ptr_;
+    std::type_index tp_index_;
 
-		return nullptr;
-	}
+}; //any class
 
-	BasePtr m_ptr;
-	std::type_index m_tpIndex;
-};
+} //wzp
 
-} // wzp
+#endif /*WZP_ANY_HPP_H_*/
