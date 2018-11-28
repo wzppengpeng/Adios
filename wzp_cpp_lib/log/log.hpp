fbb7151dbf014e01cdf7117878bc75856c4268c7
wzppengpeng <wzpycg@qq.com>
2018-11-28 19:07:30 +0800

    update the log class

--- wzp_cpp_lib/log/log.hpp
+++ wzp_cpp_lib/log/log.hpp
@@ -10,12 +10,19 @@
 
 #include "wzp_cpp_lib/common.h"
 
-namespace wzp{
+/**
+ * How to Use:
+ * as it is a staic class, just call the global function
+ * it is default as console and Info level
+ * LOG_INFO(xxx) is ok, maybe use it with wzp::format together
+ */
+
+namespace wzp {
 
 /*the type of log level*/
 enum class log_level: int{
     Fatal = -1,
-    Error = 0,
+    Warning = 0,
     Info = 1,
     Debug = 2,
 };
@@ -25,7 +32,7 @@ enum class log_type: int{
     File = 1,
 };
 
-class log{
+class log {
 public:
     /*set the log level*/
     static void set_log_level(log_level level){
@@ -55,7 +62,7 @@ public:
     template<typename... Args>
     static void debug(const std::string& format, Args&&... args){
         if(get_level() >= log_level::Debug){
-            std::string debug_str{"[Adios] [Debug] "};
+            std::string debug_str {"[Adios] [Debug] "};
             debug_str.append(format);
             write(debug_str, std::forward<Args>(args)...);
         }
@@ -64,39 +71,39 @@ public:
     template<typename... Args>
     static void info(const std::string& format, Args&&... args){
         if(get_level() >= log_level::Info){
-            std::string info_str{"[Adios] [Info] "};
+            std::string info_str {"[Adios] [Info] "};
             info_str.append(format);
             if(get_type() == log_type::Console)
-                get_stream() <<WHITE;
+                get_stream() << WHITE;
             write(info_str, std::forward<Args>(args)...);
             if(get_type() == log_type::Console)
-                get_stream() <<RESET;
+                get_stream() << RESET;
         }
     }
 
     template<typename... Args>
-    static void error(const std::string& format, Args&&... args){
-        if(get_level() >= log_level::Error){
-            std::string error_str{"[Adios] [Error] "};
+    static void warning(const std::string& format, Args&&... args){
+        if(get_level() >= log_level::Warning){
+            std::string error_str {"[Adios] [Error] "};
             error_str.append(format);
             if(get_type() == log_type::Console)
-                get_stream() <<RED;
+                get_stream() << YELLOW;
             write(error_str, std::forward<Args>(args)...);
             if(get_type() == log_type::Console)
-                get_stream() <<RESET;
+                get_stream() << RESET;
         }
     }
 
     template<typename... Args>
     static void fatal(const std::string& format, Args&&... args){
         if(get_level() >= log_level::Fatal){
-            std::string error_str{"[Adios] [Fatal] "};
+            std::string error_str {"[Adios] [Fatal] "};
             error_str.append(format);
             if(get_type() == log_type::Console)
-                get_stream() <<BOLDRED;
+                get_stream() << BOLDRED;
             write(error_str, std::forward<Args>(args)...);
             if(get_type() == log_type::Console)
-                get_stream() <<RESET;
+                get_stream() << RESET;
             exit(1);
         }
     }
@@ -104,20 +111,21 @@ public:
 private:
     /*define a global log level*/
     static log_level& get_level(){
-        static log_level level;
+        static log_level level = log_level::Info;
         return level;
     }
 
     /*define a global log type*/
     static log_type& get_type(){
-        static log_type type;
+        static log_type type = log_type::Console;
         return type;
     }
 
     /*get the output stream*/
     static std::ostream& get_stream(){
-        if(get_type() == log_type::Console) return std::cout;
-        else{
+        if(get_type() == log_type::Console)
+            return std::cout;
+        else {
             static std::ofstream outfile(get_file_path(), std::ofstream::app);
             return outfile;
         }
@@ -136,12 +144,12 @@ private:
         using std::chrono::system_clock;
         auto tp = system_clock::now();
         auto time = system_clock::to_time_t(tp);
-        std::string format{"["};
+        std::string format {"["};
         std::string str_time = std::ctime(&time);
         str_time.pop_back();
         format.append(str_time);
         format.push_back(']');
-        get_stream() << t << " " << format << std::endl;
+        get_stream() << t << ' ' << format << std::endl;
     }
 
     /*the normal write*/
@@ -179,8 +187,8 @@ private:
 #define LOG_FATAL(STR, ...) wzp::log::fatal(STR, ##__VA_ARGS__)
 #endif
 
-#ifndef LOG_ERROR
-#define LOG_ERROR(STR, ...) wzp::log::error(STR, ##__VA_ARGS__)
+#ifndef LOG_WARNING
+#define LOG_WARNING(STR, ...) wzp::log::warning(STR, ##__VA_ARGS__)
 #endif
 
 }//wzp
