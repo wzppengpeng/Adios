8ec6e45ff67cc0609e2b9da3372b1cd9e5449f90
wzppengpeng <wzpycg@qq.com>
2018-11-30 10:40:10 +0800

    add the random_uniform to get the common interface for different types (int or float)

--- wzp_cpp_lib/util/random.hpp
+++ wzp_cpp_lib/util/random.hpp
@@ -4,8 +4,17 @@
 #include <vector>
 #include <random>
 #include <algorithm>
+#include <type_traits>
 
 namespace wzp {
+
+/**
+ * How to use:
+ * 1. random_uniform(bottom, up) : input a [bottom, up] range, output a value in uniform distribution, will auto get the type needed
+ * 2. random_uniform(bottom, up, N) : input a [bottom, up] range with the vector size N, output a vector of uniform random values
+ * 3. random normal
+ * 4. random bernoulli
+ */
 
 // help function
 // random get a seed number from device
@@ -144,14 +153,12 @@ private:
  */
 template<typename Random> inline
 typename Random::res_type random(typename Random::data_type a) {
-    Random rand(a); // construct
-    return rand.Next();
+    return Random(a).Next();
 }
 
 template<typename Random> inline
 typename Random::res_type random(typename Random::data_type a, typename Random::data_type b) {
-    Random rand(a, b);
-    return rand.Next();
+    return Random(a, b).Next();
 }
 
 /**
@@ -160,15 +167,65 @@ typename Random::res_type random(typename Random::data_type a, typename Random::
  */
 template<typename Random> inline
 std::vector<typename Random::res_type> random_range(typename Random::data_type a, size_t N) {
-    Random rand(a); // construct
-    return rand.NextRange(N);
+    return Random(a).NextRange(N);
 }
 
 // the two generate type
 template<typename Random> inline
 std::vector<typename Random::res_type> random_range(typename Random::data_type a, typename Random::data_type b, size_t N) {
-    Random rand(a, b); // construct
-    return rand.NextRange(N);
+    return Radnom(a, b).NextRange(N);
+}
+
+
+/**
+ * The Help function to use Uniform Random Class
+ * judge if the input is interger or floats
+ */
+
+/**
+ * random generate a value in uniform type
+ * @param  bottom the bottom value (int or size_t)
+ * @param  up     the up value of the random (int or size_t)
+ * @return        return a random value between [bottom, up]
+ */
+template<typename T> inline
+typename std::enable_if<std::is_integral<T>::value, T>::type random_uniform(T bottom, T up) {
+    return wzp::RandomInt<T>(bottom, up).Next();
+}
+
+/**
+ * random generate a value in uniform floats type
+ * @param  bottom the bottom value (float or double)
+ * @param  up     the up value (float or double)
+ * @return        return a random value between [bottom, up]
+ */
+template<typename T> inline
+typename std::enable_if<std::is_floating_point<T>::value, T>::type random_uniform(T bottom, T up) {
+    return wzp::RandomFloats<T>(bottom, up).Next();
+}
+
+/**
+ * random generate a range value in uniform type
+ * @param  bottom the bottom value (int or size_t)
+ * @param  up     the up value of the random (int or size_t)
+ * @param   N     the size of range
+ * @return        return a random range value between [bottom, up]
+ */
+template<typename T> inline
+std::vector<typename std::enable_if<std::is_integral<T>::value, T>::type> random_uniform(T bottom, T up, size_t N) {
+    return wzp::RandomInt<T>(bottom, up).NextRange(N);
+}
+
+/**
+ * random generate a range value in uniform type
+ * @param  bottom the bottom value (float or double)
+ * @param  up     the up value of the random (float or double)
+ * @param   N     the size of range
+ * @return        return a random range value between [bottom, up]
+ */
+template<typename T> inline
+std::vector<typename std::enable_if<std::is_floating_point<T>::value, T>::type> random_uniform(T bottom, T up, size_t N) {
+    return wzp::RandomFloats<T>(bottom, up).NextRange(N);
 }
 
 } //wzp
