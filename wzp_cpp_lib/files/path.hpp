d281ce48b1d32bb92341bf12e7cc1945821724bc
wzppengpeng <wzpycg@qq.com>
2019-10-01 22:31:03 +0800

    add functions to path.hpp

--- wzp_cpp_lib/files/path.hpp
+++ wzp_cpp_lib/files/path.hpp
@@ -1,10 +1,14 @@
 #ifndef WZP_PATH_HPP
 #define WZP_PATH_HPP
 
+#include <cstdio>
+
 #include <string>
 #include <vector>
 #include <utility>
 
+#include <unistd.h>
+#include <sys/stat.h>
 
 namespace wzp
 {
@@ -18,7 +22,7 @@ public:
      * @param  paths [description]
      * @return       [description]
      */
-    inline static std::string join(const std::vector<std::string>& paths) {
+    inline static std::string Join(const std::vector<std::string>& paths) {
         //wash the strs
         std::vector<std::string> paths_(paths);
         for(auto& str : paths_) {
@@ -38,7 +42,7 @@ public:
      * @param  path [description]
      * @return      [description]
      */
-    inline static std::string basename(const std::string& path) {
+    inline static std::string Basename(const std::string& path) {
         std::string filename;
         size_t pos = path.find_last_of('/');
         if(pos != std::string::npos) filename = std::move(path.substr(pos+1));
@@ -51,7 +55,7 @@ public:
      * @param  path the raw path of the file
      * @return      the dir name
      */
-    inline static std::string dirname(const std::string& path) {
+    inline static std::string Dirname(const std::string& path) {
         std::string dir;
         size_t pos = path.find_last_of('/');
         if(pos != std::string::npos) dir = std::move(path.substr(0, pos));
@@ -63,9 +67,43 @@ public:
     /**
      * split the ext of a filename
      */
-    inline static std::pair<std::string, std::string> splitext(const std::string& filename) {
+    inline static std::pair<std::string, std::string> Splitext(const std::string& filename) {
         size_t pos = filename.find_last_of('.');
         return std::make_pair(filename.substr(0, pos), filename.substr(pos + 1));
+    }
+
+
+    /**
+     * check if exists one file
+     * @param  file string format file
+     * @return      true or false
+     */
+    inline static bool Exists(const std::string& file) {
+        return access(file.c_str(), F_OK) == 0;
+    }
+
+
+    /**
+     * check if the file is executable
+     * @param  file check file
+     * @return      true or false
+     */
+    inline static bool IsExecutable(const std::string& file) {
+        return access(file.c_str(), X_OK) == 0;
+    }
+
+
+    /**
+     * check if a path is dir
+     * @param  path the need to check path
+     * @return      true or false
+     */
+    inline static bool IsDir(const std::string& path) {
+        if(!Exists(path))
+            return false;
+        struct stat statbuf;
+        lstat(path.c_str(), &statbuf);
+        return S_ISDIR(statbuf.st_mode);
     }
 
 };
